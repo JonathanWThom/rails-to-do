be3982d7b4f0cf0eafa915fec3f0ce3b74c2e822
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-05 10:47:32 -0800

    Destroy tasks when a list is deleted

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -39,10 +39,11 @@ class ListsController < ApplicationController
 
   def destroy
     @list = List.find(params[:id])
+    @list.tasks.destroy
     @list.destroy
     redirect_to lists_path
   end
-  
+
   private
   def list_params
     params.require(:list).permit(:name)
